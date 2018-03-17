c9d582623fcc365528acc708bc256adf14396f74
Tyler Slater <tyler@tylerslater.me>
2018-03-16 21:54:36 -0600

    Block out structure

---
+++ pages/BurndownChart.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class BurndownChart extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Collect data page</Text>
+      </View>
+    );
+  }
+}
+
+BurndownChart.propTypes = {};
+BurndownChart.defaultProps = {};
+
+export default BurndownChart;
---
+++ pages/CollectData.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class CollectData extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Collect data page</Text>
+      </View>
+    );
+  }
+}
+
+CollectData.propTypes = {};
+CollectData.defaultProps = {};
+
+export default CollectData;
